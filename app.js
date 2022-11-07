015202cc6af77f5c6a44700fbbeed6dd92a24436
ianfshirley <ianfshirley88@gmail.com>
2022-11-06 19:01:09 -0800

    step 2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const theOldWay = function(course) {
   return `I am currently enrolled in ${course}`;
 };
 
-// TODO: Uncomment the following line of code to see the output in the browser console
+// DONE: Uncomment the following line of code to see the output in the browser console
 console.log('The old way:', theOldWay('Code 301'));
 
 
@@ -17,8 +17,8 @@ const theNewWay = (course) => {
   return `I am currently enrolled in ${course}`;
 };
 
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('The new way:', theNewWay('Code 301'));
+// DONE: Uncomment the following line of code to see the output in the browser console
+console.log('The new way:', theNewWay('Code 301'));
 
 
 // STEP 3
